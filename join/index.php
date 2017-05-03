fce79fc00eff470a018637ba522748f6cb877c4a
eguchiryutaro <eguchi.ryutaro@gmail.com>
2017-05-03 10:20:55 +0800

    授業でのコミット

--- join/index.php
+++ join/index.php
@@ -1,7 +1,58 @@
+<?php
+session_start();
+
+//フォームからデータがPOST送信されたとき
+if(!empty($_POST)){
+//エラー項目の確認
+//ニックネーム
+if ($_POST['nick_name'] == ''){
+    $error['nick_name'] = 'blank';
+}
+//メールアドレス
+if ($_POST['email'] == ''){
+	$error['email'] = 'blank';
+}
+//パスワード
+if ($_POST['password'] == ''){
+	$error['password'] = 'blank';
+}else if (strlen($_POST['password']) < 4){
+	$error['password'] = 'length';
+}
+
+//画像ファイルの拡張子シェック($_FILES)
+$filename = $_FILES['picture_path']['name'];
+if (!empty($filename)){
+
+  //拡張子を取得
+  $ext = substr($filename, -3) ;
+  $ext = strtolower($ext);
+
+  if($ext != 'jpg' && $ext != 'gif' && $ext != 'png'){
+    $error['picyure_path'] = 'type';
+  }
+}
+
+//エラーが出ない場合
+if (empty($error)){
+
+  $picture_path = date('YmdHis') . $_FILES['picture_path']['name'];
+  move_uploaded_file($_FILES['picture_path']['tmp_name'], '../member_picture/' . $picture_path);
+
+
+  //セッションに値を保存
+  $_SESSION['join'] = $_POST;
+  $_SESSION['join']['picture_path'] = $picture_path;
+
+	header('Location: check.php');
+}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-    <meta charset="utf-8">
+   <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -45,26 +96,55 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3 content-margin-top">
         <legend>会員登録</legend>
-        <form method="post" action="" class="form-horizontal" role="form">
+        <form method="post" action="" class="form-horizontal" role="form" enctype="multipart/form-data">
           <!-- ニックネーム -->
           <div class="form-group">
             <label class="col-sm-4 control-label">ニックネーム</label>
             <div class="col-sm-8">
-              <input type="text" name="nick_name" class="form-control" placeholder="例： Seed kun">
+            <?php if (isset($_POST['nick_name'])): ?>
+              <input type="text" name="nick_name" class="form-control" placeholder="例： Seedkun" value="<?php echo htmlspecialchars($_POST['nick_name'], ENT_QUOTES, 'UTF-8'); ?>">
+          <?php else: ?>
+          <input type="nick_name" name="nick_name" class="form-control" placeholder="例： seedkun" value="">
+          <?php endif; ?>
+               <?php if (isset($error['nick_name']) && $error['nick_name'] == 'blank'){
+              ?>
+              <p class="error">*ニックネームを入力してください。</p>
+              <?php } ?>
             </div>
           </div>
           <!-- メールアドレス -->
           <div class="form-group">
             <label class="col-sm-4 control-label">メールアドレス</label>
             <div class="col-sm-8">
-              <input type="email" name="email" class="form-control" placeholder="例： seed@nex.com">
+            <?php if (isset($_POST['email'])): ?>
+          <input type="email" name="email" class="form-control" placeholder="例： seed@nex.com" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8'); ?>">
+          <?php else: ?>
+          <input type="email" name="email" class="form-control" placeholder="例： seed@nex.com" value="">
+          <?php endif; ?>
+               <?php if (isset($error['email']) && $error['email'] == 'blank'){
+              ?>
+              <p class="error">*メールアドレスを入力してください。</p>
+              <?php } ?>
             </div>
           </div>
           <!-- パスワード -->
           <div class="form-group">
             <label class="col-sm-4 control-label">パスワード</label>
             <div class="col-sm-8">
-              <input type="password" name="password" class="form-control" placeholder="">
+             
+              <?php if (isset($_POST['password'])): ?><input type="password" name="password" class="form-control" placeholder="" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8'); ?>">
+
+                <?php else: ?>
+                      <input type="password" name="password" class="form-control" placeholder="" value="">
+                <?php endif; ?>
+               <?php if (isset($error['password']) && $error['password'] == 'blank'){
+              ?>
+              <p class="error">*パスワードを入力してください。</p>
+              <?php } ?>
+        	  <?php if (isset($error['password']) && $error['password'] == 'length'){
+        	  ?>
+              <p class="error">*パスワードは４文字以上で入力してください。</p>
+              <?php } ?>
             </div>
           </div>
           <!-- プロフィール写真 -->
@@ -72,6 +152,12 @@
             <label class="col-sm-4 control-label">プロフィール写真</label>
             <div class="col-sm-8">
               <input type="file" name="picture_path" class="form-control">
+              <?php if (isset($error['picture_path']) && $eroor['picture_path'] == 'type'): ?>
+                <p class="error">=写真などは「.gif」「.jpg」「.png」の画像を指定してください。</p>
+              <?php endif; ?>
+              <?php if (!empty($error)): ?>
+                <p class="error">= 恐れ入りますが、画像を改めて指定してください。</p>
+                <?php endif; ?>
             </div>
           </div>
 
